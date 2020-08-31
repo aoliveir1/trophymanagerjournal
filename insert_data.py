a752a00bb072760bca720473d417d67da334c11f
André <oioi-skin84@hotmail.com>
2020-08-31 13:51:53 -0300

    Update insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -9,9 +9,9 @@ from splinter.exceptions import ElementDoesNotExist
 
 from utils import create_brower, sign_in
 
-conn = sqlite3.connect('tmjournal_season62.db')
+conn = sqlite3.connect('tmjournal_season64.db')
 cursor = conn.cursor()
-conn_players = sqlite3.connect('rating_players_season62.db')
+conn_players = sqlite3.connect('rating_players_season64.db')
 cursor_players = conn_players.cursor()
 
 
@@ -20,62 +20,60 @@ def get_table_list(index: int) -> list:
     Index 0 for all tables. Index 1 to 14 for the tables contained in the list."""
     db_tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
     all_tables = [table[0] for table in db_tables]
+    all_tables.remove('brazil_2_1')
+    all_tables.remove('brazil_2_2')
+    all_tables.remove('brazil_2_3')
+    all_tables.remove('brazil_2_4')
 
     # 00:00
-    prev1 = ['fixtures_paraguay', 'fixtures_panama', 'fixtures_cuba', 'fixtures_venezuela', 'fixtures_ecuador',
-             'fixtures_dominican_republic', 'fixtures_trinidad___tobago', 'fixtures_west_indies', 'fixtures_colombia']
+    prev1 = ['paraguay', 'panama', 'cuba', 'venezuela', 'ecuador', 'dominican_republic', 'trinidad___tobago',
+             'west_indies', 'colombia']
 
     # 00:00, 01:00
-    prev2 = ['fixtures_bolivia', 'fixtures_peru', 'fixtures_jamaica', 'fixtures_mexico', 'fixtures_puerto_rico',
-             'fixtures_canada', 'fixtures_honduras', 'fixtures_belize', 'fixtures_el_salvador']
+    prev2 = ['bolivia', 'peru', 'jamaica', 'mexico', 'puerto_rico', 'canada', 'honduras', 'belize', 'el_salvador']
 
     # 01:00, 08:00, 10:00, 11:00
-    prev3 = ['fixtures_united_states', 'fixtures_guatemala', 'fixtures_costa_rica', 'fixtures_new_zealand',
-             'fixtures_fiji', 'fixtures_oceania', 'fixtures_australia', 'fixtures_hong_kong', 'fixtures_singapore']
+    prev3 = ['united_states', 'guatemala', 'costa_rica', 'new_zealand', 'fiji', 'oceania', 'australia', 'hong_kong',
+             'singapore']
 
     # 11:00, 12:00
-    prev4 = ['fixtures_philippines', 'fixtures_brunei_darussalam', 'fixtures_taiwan', 'fixtures_chinese_beijing',
-             'fixtures_malaysia', 'fixtures_japan', 'fixtures_south_korea', 'fixtures_indonesia', 'fixtures_thailand']
+    prev4 = ['philippines', 'brunei_darussalam', 'taiwan', 'chinese_beijing', 'malaysia', 'japan', 'south_korea',
+             'indonesia', 'thailand']
 
     # 12:00, 13:00, 14:00
-    prev5 = ['fixtures_vietnam', 'fixtures_kazakhstan', 'fixtures_nepal', 'fixtures_india', 'fixtures_bangladesh',
-             'fixtures_afghanistan', 'fixtures_pakistan', 'fixtures_united_arab_emirates', 'fixtures_azerbaijan']
+    prev5 = ['vietnam', 'kazakhstan', 'nepal', 'india', 'bangladesh', 'afghanistan', 'pakistan', 'united_arab_emirates',
+             'azerbaijan']
 
     # 14:00, 15:00
-    prev6 = ['fixtures_oman', 'fixtures_iran', 'fixtures_georgia', 'fixtures_armenia', 'fixtures_qatar',
-             'fixtures_ukraine', 'fixtures_turkey', 'fixtures_palestine', 'fixtures_kuwait']
+    prev6 = ['oman', 'iran', 'georgia', 'armenia', 'qatar', 'ukraine', 'turkey', 'palestine', 'kuwait']
 
     # 15:00
-    prev7 = ['fixtures_saudi_arabia', 'fixtures_lebanon', 'fixtures_jordan', 'fixtures_bahrain', 'fixtures_moldova',
-             'fixtures_russia', 'fixtures_romania', 'fixtures_estonia', 'fixtures_bulgaria']
+    prev7 = ['saudi_arabia', 'lebanon', 'jordan', 'bahrain', 'moldova', 'russia', 'romania', 'estonia', 'bulgaria']
 
     # 15:00, 16:00
-    prev8 = ['fixtures_iraq', 'fixtures_belarus', 'fixtures_lithuania', 'fixtures_syria', 'fixtures_latvia',
-             'fixtures_bosnia_herzegovina', 'fixtures_croatia', 'fixtures_finland', 'fixtures_montenegro']
+    prev8 = ['iraq', 'belarus', 'lithuania', 'syria', 'latvia', 'bosnia_herzegovina', 'croatia', 'finland',
+             'montenegro']
 
     # 16:00
-    prev9 = ['fixtures_hungary', 'fixtures_czech_republic', 'fixtures_greece', 'fixtures_egypt', 'fixtures_poland',
-             'fixtures_slovenia', 'fixtures_libya', 'fixtures_botswana', 'fixtures_cyprus', 'fixtures_slovakia']
+    prev9 = ['hungary', 'czech_republic', 'greece', 'egypt', 'poland', 'slovenia', 'libya', 'botswana', 'cyprus']
 
     # 16:00, 18:00
-    prev10 = ['fixtures_israel', 'fixtures_serbia', 'fixtures_north_macedonia', 'fixtures_albania', 'fixtures_cameroun',
-              'fixtures_belgium', 'fixtures_south_africa', 'fixtures_switzerland', 'fixtures_norway']
+    prev10 = ['slovakia', 'israel', 'serbia', 'north_macedonia', 'albania', 'cameroun', 'belgium', 'south_africa',
+              'switzerland']
 
     # 18:00
-    prev11 = ['fixtures_malta', 'fixtures_denmark', 'fixtures_sweden', 'fixtures_austria', 'fixtures_spain',
-              'fixtures_san_marino', 'fixtures_france', 'fixtures_germany', 'fixtures_andorra']
+    prev11 = ['malta', 'denmark', 'sweden', 'austria', 'spain', 'san_marino', 'france', 'germany', 'andorra']
 
     # 18:00, 20:00
-    prev12 = ['fixtures_italy', 'fixtures_netherlands', 'fixtures_luxembourg', 'fixtures_portugal', 'fixtures_ireland',
-              'fixtures_algeria', 'fixtures_ghana', 'fixtures_cote_d_ivoire', 'fixtures_northern_ireland']
+    prev12 = ['norway', 'italy', 'netherlands', 'luxembourg', 'portugal', 'ireland', 'algeria', 'ghana',
+              'cote_d_ivoire']
+
+    # 20:00
+    prev13 = ['northern_ireland', 'nigeria', 'angola', 'iceland', 'scotland', 'faroe_islands', 'chad', 'wales',
+              'morocco']
 
     # 20:00, 21:00
-    prev13 = ['fixtures_nigeria', 'fixtures_angola', 'fixtures_iceland', 'fixtures_scotland', 'fixtures_faroe_islands',
-              'fixtures_chad', 'fixtures_wales', 'fixtures_morocco', 'fixtures_senegal']
-
-    # 20:00, 21:00
-    prev14 = ['fixtures_england', 'fixtures_tunisia', 'fixtures_chile', 'fixtures_argentina', 'fixtures_brazil',
-              'fixtures_uruguay']
+    prev14 = ['senegal', 'england', 'tunisia', 'chile', 'argentina', 'brazil', 'uruguay']
 
     table_list = (all_tables, prev1, prev2, prev3, prev4, prev5, prev6, prev7, prev8, prev9, prev10, prev11, prev12,
                   prev13, prev14)
@@ -84,7 +82,7 @@ def get_table_list(index: int) -> list:
 
 
 def update_matches_table(fixture_data: list) -> str:
-    """Formats a query."""
+    """Formats a query for update matches tables."""
     table_name = fixture_data[0]
     attendance = fixture_data[1]
     stadium = fixture_data[2]
@@ -104,15 +102,15 @@ def update_matches_table(fixture_data: list) -> str:
 
 
 def create_players_table(table_name: str) -> str:
-    """Formats a query."""
+    """Formats a query for create table if not exists."""
     create_table = '''CREATE TABLE IF NOT EXISTS {} (player_name TEXT, player_age TEXT, player_wage FLOAT, 
     player_position TEXT, player_nation TEXT, player_link TEXT, player_si INTEGER, player_rating FLOAT, player_goals 
-    INTEGER, turn INTEGER, turn_link TEXT, team TEXT, team_link TEXT)'''.format(table_name)
+    INTEGER, turn INTEGER, turn_link TEXT, team TEXT, team_link TEXT, team_nation TEXT)'''.format(table_name)
     return create_table
 
 
 def insert_players_data(players_data: list) -> str:
-    """Formats a query."""
+    """Formats a query for insert."""
     table = players_data[0]
     name = players_data[1]
     age = players_data[2]
@@ -125,27 +123,29 @@ def insert_players_data(players_data: list) -> str:
     team = players_data[9]
     team_link = players_data[10]
     turn = players_data[11]
-    nation = players_data[12]
+    player_nation = players_data[12]
+    team_nation = players_data[13]
     insert_players_data = '''INSERT INTO {} (player_name, player_age, player_wage, player_link, player_si, 
-    player_position, player_rating, player_goals, team, team_link, turn, player_nation) VALUES ("{}", {}, {}, "{}", {}, 
-    "{}", {}, {}, "{}", "{}", {}, "{}");'''.\
-        format(table, name, age, wage, link, si, position, rating, goals, team, team_link, turn, nation)
+    player_position, player_rating, player_goals, team, team_link, turn, player_nation, team_nation) VALUES 
+    ("{}", "{}", {}, "{}", {}, "{}", {}, {}, "{}", "{}", {}, "{}", "{}");'''.\
+        format(table, name, age, wage, link, si, position, rating, goals, team, team_link, turn, player_nation, team_nation)
     return insert_players_data
 
 
 def get_match_data(link):
     """Receive the match link and scrape for the data."""
-    # link = '/matches/148692932/'  #  Forfeit example
-    # link = '/matches/148693198/'  #  April match
     try:
         players = []
         url_match = url_base + link
         browser.visit(url_match)
         browser.visit(url_match)
 
+        # base_xpath = '/html/body/div[8]/div[2]'  # non pro
+        base_xpath = '/html/body/div[5]/div[2]'  # with pro
+
         # Go to report is a button that appears when the game ends
         while browser.is_element_not_present_by_css('.go_to_report'):
-            time.sleep(3)
+            time.sleep(5)
             if browser.is_element_not_present_by_css('.field > div:nth-child(1)'):
                 attendance = -1
                 stadium = ''
@@ -162,18 +162,20 @@ def get_match_data(link):
             attendance = attendance.text.strip()
             attendance = attendance.replace(',', '')
 
-            stadium = browser.find_by_xpath('/html/body/div[8]/div[2]/div/div[10]/div/ul[1]/li[1]').first
+            # '/html/body/div[8]/div[2]/div/div[10]/div/ul[1]/li[1]'
+            stadium = browser.find_by_xpath(base_xpath + '/div/div[10]/div/ul[1]/li[1]').first
             stadium = stadium.text.strip().replace('“', '\'').replace('"', '\'').replace('¨', '\'')
-
-        while browser.is_element_not_present_by_css('div.score'):
-            print('.', end='')
-        scoreboard = browser.find_by_css('div.score').first
-        scoreboard = scoreboard.text.strip()
-        scoreboard = scoreboard.split('-')
-        home_team_score, away_team_score = [s.strip() for s in scoreboard]
-
         try:
-            home_team = browser.find_by_xpath('/html/body/div[8]/div[1]/div[3]/div/div[2]/div[2]/a').first
+            while browser.is_element_not_present_by_css('div.score'):
+                print('.', end='')
+            scoreboard = browser.find_by_css('div.score').first
+            scoreboard = scoreboard.text.strip()
+            scoreboard = scoreboard.split('-')
+            home_team_score, away_team_score = [s.strip() for s in scoreboard]
+        except:
+            return None
+        try:
+            home_team = browser.find_by_xpath('/html/body/div[5]/div/div[3]/div/div[2]/div[2]/a').first
             home_team_link = browser.find_by_css('div.names:nth-child(2) > a:nth-child(1)').first
             home_team_link = home_team_link['href']
 
@@ -181,12 +183,12 @@ def get_match_data(link):
             away_team_link = browser.find_by_css('div.names:nth-child(4) > a:nth-child(1)').first
             away_team_link = away_team_link['href']
         except:
-            home_team = browser.find_by_css(
-                'body > div:nth-child(9) > div.box_body.mv_bottom > div > div:nth-child(6) > div > div.nameplate > div.overlay > div.home.name.bold').first
-            home_team_link = 'https://trophymanager.com/club/' + home_team['club_link']
-            away_team = browser.find_by_css(
-                'body > div:nth-child(9) > div.box_body.mv_bottom > div > div:nth-child(6) > div > div.nameplate > div.overlay > div.away.name.bold')
-            away_team_link = 'https://trophymanager.com/club/' + away_team['club_link']
+            home_team = browser.find_by_xpath(base_xpath + '/div/div[6]/div/div[1]/div[3]/div[3]').first
+            id = home_team['club_link']
+            home_team_link = 'https://trophymanager.com/club/' + id
+            away_team = browser.find_by_xpath(base_xpath + '/div/div[6]/div/div[1]/div[3]/div[2]').first
+            id = away_team['club_link']
+            away_team_link = 'https://trophymanager.com/club/' + id
 
         home_team = home_team.text.strip().replace('“', '\'').replace('"', '\'')
         away_team = away_team.text.strip().replace('“', '\'').replace('"', '\'')
@@ -207,61 +209,94 @@ def get_match_data(link):
             for item in player:
                 href = item.a['href']
                 link = 'https://trophymanager.com' + href
-
-                player_nation = ''
-                age = 0
-                wage = 0
-                si = 0
-
-                # '''Player SI'''
-                browser.visit(link)
-                try:
-                    player_nation = BeautifulSoup(browser.html, 'html.parser')
-                    player_nation = player_nation.find('div', {'class': 'large'})
-                    player_nation = player_nation.find('a', {'class': 'country_link'})
-                    player_nation = player_nation['href']
-                    player_nation = player_nation.replace('/national-teams/', '').replace('/', '')
-                except:
-                    player_nation = ''
-                try:
-                    age = browser.find_by_css(
-                        'body > div:nth-child(9) > div.column2_a > div > div.box_body > div:nth-child(4) > table > tbody > tr:nth-child(3) > td')
-                    age = age.text
-                    age = age.replace(' Years ', '.').replace('Months', '')
-                except:
-                    age = 0
-                try:
-                    wage = browser.find_by_css(
-                        'body > div:nth-child(9) > div.column2_a > div > div.box_body > div:nth-child(4) > table > tbody > tr:nth-child(5) > td > span')
-                    wage = wage.text
-                    wage = wage.replace(',', '')
-                    wage = int(wage)
-                except:
-                    wage = 0
-                try:
-                    si = browser.find_by_css('.float_left > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2)')
-                    si = si.text
-                    si = si.replace(',', '')
-                    si = int(si)
-                except:
-                    si = 0
-                # ''' End Player SI'''
-
+                t_name = href.replace('/players/', 't_')
                 name = item.find('div', {'class': 'name'})
                 name = name.text
+                position = item.find('div', {'class': 'position'})
+                position = position.text
                 try:
                     rating = item.find('div', {'class': 'rating'})
                     rating = float(rating.text)
                 except:
                     rating = 0
-                position = item.find('div', {'class': 'position'})
-                position = position.text
                 goal = item.findAll('img', {'src': '/pics/icons/ball.gif'})
 
-                player = [name, age, wage, link, si, position, rating, len(goal), team_name, team_link, player_nation]
+                player_nation = ''
+                team_nation = ''
+                age = 0
+                wage = 0
+                si = 0
+
+                '''Player SI'''
+                '/html/body/div[8]/div[2]/div/div[2]/div[2]/div/strong'
+                if t_name in set_tables():
+                    print('player in set tables')
+                    try:
+                        browser.visit(link)
+                        name = browser.find_by_xpath(base_xpath + '/div/div[2]/div[2]/div/strong').first
+                        name = name.text
+                        p = name.find('.')
+                        name = name[p+2:]
+                    except:
+                        # print('não peguei o nome no try')
+                        pass
+                    try:
+                        position = browser.find_by_xpath(base_xpath + '/div[2]/div/div[2]/div[2]/div/span[2]/strong/span/span').first
+                        position = position['class']
+                    except:
+                        # print('não peguei a posição no try')
+                        pass
+                    try:
+                        position2 = browser.find_by_xpath(
+                            base_xpath + '/div/div[2]/div[2]/div/span[2]/strong/span/span[3]').first
+                        position2 = position2['class']
+                        if position2 != 'split':
+                            position = position + '/' + position2
+                    except:
+                        pass
+                    try:
+                        player_nation = BeautifulSoup(browser.html, 'html.parser')
+                        player_nation = player_nation.find('div', {'class': 'large'})
+                        player_nation = player_nation.find('a', {'class': 'country_link'})
+                        player_nation = player_nation['href']
+                        player_nation = player_nation.replace('/national-teams/', '').replace('/', '')
+                    except:
+                        player_nation = ''
+                    try:
+                        team_nation = browser.find_by_xpath(base_xpath + '/div/div[2]/div[4]/table/tbody/tr[2]/td/a[2]').first
+                        team_nation = team_nation['href']
+                        team_nation = team_nation[-3:-1]
+                    except:
+                        team_nation = ''
+                        # input('cant find team nation, ' + str(item))
+                    try:
+                        age = browser.find_by_xpath(base_xpath + '/div/div[2]/div[4]/table/tbody/tr[3]/td')
+                        age = age.text
+                        age = age.replace(' Years ', '.').replace('Months', '')
+                        age = age.replace(' Anos ', '.').replace('Meses', '')
+                    except:
+                        age = 0
+                    try:
+                        wage = browser.find_by_xpath(base_xpath + '/div/div[2]/div[4]/table/tbody/tr[5]/td/span')
+                        wage = wage.text
+                        wage = wage.replace(',', '')
+                        wage = int(wage)
+                    except:
+                        wage = 0
+                    try:
+                        si = browser.find_by_css('.float_left > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2)')
+                        si = si.text
+                        si = si.replace(',', '')
+                        si = int(si)
+                    except:
+                        si = 0
+                ''' End Player SI'''
+
+                player = [name, age, wage, link, si, position, rating, len(goal), team_name, team_link, player_nation, team_nation]
                 players.append(player)
 
-        return attendance, stadium, home_team, home_team_score, home_team_link, away_team, away_team_score, away_team_link, players
+        return [attendance, stadium, home_team, home_team_score, home_team_link, away_team, away_team_score,
+                away_team_link, players]
     except (InvalidSessionIdException, NoSuchWindowException, WebDriverException) as e:
         print(e.msg)
         return None
@@ -325,6 +360,3 @@ for table in tables:
 conn_players.close()
 conn.close()
 browser.quit()
-
-if tables_fails:
-    print(tables_fails)
